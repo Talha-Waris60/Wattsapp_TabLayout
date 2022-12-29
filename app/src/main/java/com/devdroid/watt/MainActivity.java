810d239bc3a2b8da8c78068b1b925b43a1cc28d3
Talha Waris <victormalik60@gmail.com>
2022-12-29 13:29:09 +0500

    Test Commit

--- app/src/main/java/com/devdroid/watt/MainActivity.java
+++ app/src/main/java/com/devdroid/watt/MainActivity.java
@@ -1,14 +1,62 @@
 package com.devdroid.watt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    FrameLayout frameLayout;
+    TabLayout tabLayout;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tabLayout = findViewById(R.id.tabLayout);
+        frameLayout = findViewById(R.id.fragContainer);
+        loadFragment(new chatFragment());
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition())
+                {
+                    case 0:
+                        loadFragment(new CameraFragment());
+                        break;
+                    case 1:
+                        loadFragment(new chatFragment());
+                        break;
+                    case 2:
+                        loadFragment(new StatusFragment());
+                        break;
+                    case 3:
+                        loadFragment(new CallFragment());
+                }
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    public void loadFragment(Fragment fragment)
+    {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragContainer,fragment).commit();
     }
 }
